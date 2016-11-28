3946dc6fac0980ed879d56f5d68c4e9135b30c06
admin <admin@localhost.localdomain>
2016-11-28 22:45:08 +0530

    included UI separations, styles and other UI contents

---
+++ src/main/java/lk/health/phd/cd/controllers/UiController.java
@@ -0,0 +1,30 @@
+package lk.health.phd.cd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UiController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getIndexPage(Model model) {
+		return "dashboard";
+	}
+
+	@RequestMapping(value = "/page/footer", method = RequestMethod.GET)
+	public String getFooter() {
+		return "/common/footer";
+	}
+
+	@RequestMapping(value = "/page/banner", method = RequestMethod.GET)
+	public String getBanner() {
+		return "/common/banner";
+	}
+
+	@RequestMapping(value = "/page/navigation", method = RequestMethod.GET)
+	public String getNavigation() {
+		return "/common/navigation";
+	}
+}
--- src/main/webapp/js/form544-custom.js
+++ src/main/webapp/js/form544-custom.js
@@ -9,3 +9,19 @@ $(function() {
 		dateFormat : 'yy-mm-dd'
 	});
 });
+
+$(function() {
+	$("#footer").load("/communicable-disease/page/footer");
+});
+
+$(function() {
+	$("#banner").load("/communicable-disease/page/banner");
+});
+
+$(function() {
+	$("#navigation").load("/communicable-disease/page/navigation");
+});
+
+function setPageName(pageName) {
+	document.getElementById('pageName').innerHTML = pageName;
+}
