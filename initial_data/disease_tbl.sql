a84f46c353e30da35d39fb80c7869132a2be54d6
admin <admin@localhost.localdomain>
2016-12-30 21:54:56 +0530

    included diseae initial sql dump

---
+++ initial_data/disease_tbl.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.6.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: communicable_disease
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.0.23-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `disease`
+--
+
+DROP TABLE IF EXISTS `disease`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `disease` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `disease_name` varchar(255) NOT NULL,
+  `shortcode` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `disease`
+--
+
+LOCK TABLES `disease` WRITE;
+/*!40000 ALTER TABLE `disease` DISABLE KEYS */;
+INSERT INTO `disease` VALUES (1,'Cholera','CHO'),(2,'Plague','PL'),(3,'Yellow Fever','YF'),(4,'Acute Poliomyclitis','AP'),(5,'Chicken pox','CP'),(6,'Dengue Fever','DF'),(7,'Diptheria','DPT'),(8,'Dysentary','DYS'),(9,'Encephalitis','EN'),(10,'Enteric Fever','EF'),(11,'Food Poisonning','FP'),(12,'Human Rabies','HR'),(13,'Leptospirosis','LE'),(14,'Malaria','MA'),(15,'Measles','ME'),(16,'Meningitis','MEN'),(17,'Mumps','MU'),(18,'Rubella','RU'),(19,'Simple Continued Fever of over 7days or more','SCF'),(20,'Tetanus','TE'),(21,'Neonatal Tetanus','NT'),(22,'Typhus Fever','TF'),(23,'Viral Hepatitis','VH'),(24,'Tuberculosis','TB');
+/*!40000 ALTER TABLE `disease` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-30 21:48:51
