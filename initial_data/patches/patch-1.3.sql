c564da0e2896b90ac11e40bc74ccd8c40b12ad5a
admin <admin@localhost.localdomain>
2017-11-18 22:25:47 +0530

    removed DB disease duplicates, added new 2 diseases

--- initial_data/patches/patch-1.3.sql
+++ initial_data/patches/patch-1.3.sql
@@ -8,3 +8,12 @@ update form_544 set institute=1 where institute='District General Hospital - Mat
 
 alter table form_544 drop column institute_id;
 alter table form_544 change `institute` `institute_id` bigint(20);
+
+update form_544 set disease_id=6 where disease_id=26;
+delete from disease where id=26 and disease_name='Dengue Fever';
+
+update form_544 set disease_id=25 where disease_id=27;
+delete from disease where id=27 and disease_name='Influenza H1N1';
+
+insert into disease(disease_name, shortcode) values('Blood and Mucosa Diarrhoea', 'BMD');
+insert into disease(disease_name, shortcode) values('Leishmenaisis and Leprosy', 'LL');
