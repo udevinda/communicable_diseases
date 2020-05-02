69d57239ab0b11f8d318155e1d48bc78a630d95d
Udara Seneviratne <udara.seneviratne@hsenidmobile.com>
2020-05-02 17:25:40 +0530

    included a master data file

---
+++ initial_data/ward.sql
@@ -0,0 +1,8 @@
+insert into ward(name, institute_id) values("03", 1);
+insert into ward(name, institute_id) values("04", 1);
+insert into ward(name, institute_id) values("06", 1);
+insert into ward(name, institute_id) values("07", 1);
+insert into ward(name, institute_id) values("08", 1);
+insert into ward(name, institute_id) values("09", 1);
+insert into ward(name, institute_id) values("CLINIC", 1);
+insert into ward(name, institute_id) values("ICU", 1);
